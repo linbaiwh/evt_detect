e029d46ba020904190af9b32c27ed86a30e0dfc8
Lin <linbaiwh@gmail.com>
2021-06-22 01:14:08 -0400

    transformers set up and test

---
+++ evt_detect/models/trfs.py
@@ -0,0 +1,51 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+import evt_detect.features.nlp_features as nlp_feat
+
+class trf_length(BaseEstimator, TransformerMixin):
+    def __init__(self, tokenizer=None):
+        self.tokenizer = tokenizer
+        self.features = None
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        df = nlp_feat.length_feature(X, tokenizer=self.tokenizer).drop('sents', axis=1)
+        self.features = df.columns
+        return df
+
+    def get_feature_names(self):
+        return self.features.tolist()
+
+
+class trf_pos(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.features = None
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        df = nlp_feat.pos_feature(X).drop('sents', axis=1)
+        self.features = df.columns
+        return df
+
+    def get_feature_names(self):
+        return self.features.tolist()
+
+
+class trf_sentiment(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.features = None
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        df = nlp_feat.sentiment_feature(X).drop('sents', axis=1)
+        self.features = df.columns
+        return df
+
+    def get_feature_names(self):
+        return self.features.tolist()
